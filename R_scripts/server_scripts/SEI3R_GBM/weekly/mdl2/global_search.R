2840bc518193f311143408b9852d1b3efabf3c68
Jair Andrade <jair.albert.andrade@gmail.com>
2021-03-05 16:34:06 +0000

    Prepare Global search GBM weekly mdl 2 dt 1/128

---
+++ R_scripts/server_scripts/SEI3R_GBM/weekly/mdl2/global_search.R
@@ -0,0 +1,35 @@
+library(deSolve)
+library(doParallel)
+library(doRNG)
+library(dplyr)
+library(imputeTS)
+library(lubridate)
+library(pomp)
+library(purrr)
+library(readr)
+library(readxl)
+library(stringr)
+library(tictoc)
+library(tidyr)
+
+folder <- "./Saved_objects/Irish_data/SEI3R_GBM/weekly/model_2"
+
+fn     <- file.path(folder, "local_search_mdl2.rds")
+ls_obj <- readRDS(fn)
+
+mf1          <- ls_obj$result[[1]] 
+par_obj      <- get_params("GBM_2")
+fixed_params <- par_obj$fixed      
+
+set.seed(76393492)
+
+runif_design(
+  lower = c(zeta = 0.1, P_0 = 1,  tau = 0.05, alpha = 0.05),
+  upper = c(zeta = 3,   P_0 = 30, tau = 3, alpha = 0.3),
+  nseq  = 300
+) -> guesses
+
+fn     <- file.path(folder, "Global_search_mdl2.rds")
+seed   <- 971112215
+source("./R_scripts/global_search.R")
+gs_obj <- global_search(guesses, fixed_params, mf1, fn, seed, 7)
