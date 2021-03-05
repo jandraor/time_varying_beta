7f6a984d18f2c9b3ccffe7e5801a132e966324d4
Jair Andrade <jair.albert.andrade@gmail.com>
2021-03-05 16:49:25 +0000

    Update global_search.R

--- R_scripts/server_scripts/SEI3R_GBM/weekly/mdl2/global_search.R
+++ R_scripts/server_scripts/SEI3R_GBM/weekly/mdl2/global_search.R
@@ -18,6 +18,7 @@ fn     <- file.path(folder, "local_search_mdl2.rds")
 ls_obj <- readRDS(fn)
 
 mf1          <- ls_obj$result[[1]] 
+source("./R_scripts/POMP_models.R")
 par_obj      <- get_params("GBM_2")
 fixed_params <- par_obj$fixed      
 
