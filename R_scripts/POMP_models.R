f00b9d192350c101c569ea4963d32f3c13c5fdd9
Jair Andrade <jair.albert.andrade@gmail.com>
2021-03-16 16:49:11 +0000

    Remove old results & fix N

--- R_scripts/POMP_models.R
+++ R_scripts/POMP_models.R
@@ -13,11 +13,13 @@ get_fixed_params <- function() {
 
 get_params <- function(mdl_id) {
   
-  zeta_guess  <- 3 / 5
-  tau_guess   <- 1
-  B_guess     <- zeta_guess
-  P_guess     <- 1
-  alpha_guess <- 0.1
+  zeta_guess    <- 3 / 5
+  tau_guess     <- 1
+  B_guess       <- zeta_guess
+  P_guess       <- 1
+  alpha_guess   <- 0.1
+  nu_guess      <- 0.04
+  upsilon_guess <- 0.1
   
   if(mdl_id == "GBM_1") {
     unknown_pars <- c(B_0 = B_guess, P_0 = P_guess, alpha = alpha_guess)
@@ -29,8 +31,6 @@ get_params <- function(mdl_id) {
   }
   
   if(mdl_id == "CIR") {
-    nu_guess      <- 0.04
-    upsilon_guess <- 0.1
     unknown_pars  <- c(zeta = zeta_guess, P_0 = P_guess, tau = tau_guess,
                        nu = nu_guess, upsilon = upsilon_guess,
                        alpha = alpha_guess)
@@ -110,7 +110,7 @@ pomp_SEI3R_GBM2 <- function(obs_df, params, dt = 0.01) {
   ") -> dmeas
   
   Csnippet("
-    S = 4999970 - P_0;
+    S = N - P_0;
     E = 0;
     P = P_0;
     I = 0;
@@ -157,70 +157,15 @@ pomp_SEI3R_GBM2 <- function(obs_df, params, dt = 0.01) {
 pomp_SEI3R_CIR <- function(obs_df, pars, dt = 0.01) {
   Csnippet("
     y1  = rpois(C);  
-    y2 = rnorm(B, tau);
-  ") -> rmeas
-  
-  Csnippet("
-    lik = dpois(y1,C,give_log) + dnorm(y2, B, tau, give_log);
-  ") -> dmeas
-  
-  Csnippet("
-    S = 4999970 - P_0;
-    E = 0;
-    P = P_0;
-    I = 0;
-    A = 0;
-    R = 0;
-    B = 1;
-    C = 0;
-  ") -> rinit
-  
-  Csnippet("
-    double dW     = rnorm(0,sqrt(dt));
-    double lambda = zeta * B * (I + P + mu * A) / N;
-    S-= (S * lambda)*dt;
-    E+= (S * lambda - sigma*E)*dt;
-    P+= (omega * sigma * E - eta * P) * dt;
-    I+= (eta * P - gamma*I)*dt;
-    R+= (gamma*I + kappa * A)*dt;
-    A+= ((1-omega) * sigma * E - kappa * A) * dt;
-    B+= (nu * (upsilon - B)) * dt + sqrt(alpha)*B*dW;
-    C+= (rho*eta*P)*dt;
-  ") -> SEI3R_CIR_step
-  
-  par_names <- names(pars)
-  
-  obs_df %>% 
-    pomp(
-      times = "time", t0 = 0,
-      rinit = rinit,
-      rprocess = pomp::euler(SEI3R_CIR_step, delta.t = dt),
-      statenames = c("S", "E", "P","I", "R", "A", "C", "B"),
-      paramnames = par_names,
-      params = params,
-      accumvars = "C",
-      rmeasure = rmeas,
-      dmeasure = dmeas,
-      partrans = parameter_trans(log   = c("zeta", "P_0", "tau", "nu", 
-                                           "upsilon", "alpha")),
-      obsnames = c("y1", "y2"),
-      cdir = ".", 
-      cfile= "SEI3R_CIR"
-    ) -> SEI3R_CIR
-}
-
-pomp_SEI3R_GBM2_binom <- function(obs_df, params, dt = 0.01) {
-  Csnippet("
-    y1  = rbinom(C, rho);
     y2 = rnorm(Z, tau);
   ") -> rmeas
   
   Csnippet("
-    lik = dbinom(y1,round(C),rho,give_log) + dnorm(y2, Z, tau, give_log);
+    lik = dpois(y1,C,give_log) + dnorm(y2, Z, tau, give_log);
   ") -> dmeas
   
   Csnippet("
-    S = 4999970 - P_0;
+    S = N - P_0;
     E = 0;
     P = P_0;
     I = 0;
@@ -239,30 +184,27 @@ pomp_SEI3R_GBM2_binom <- function(obs_df, params, dt = 0.01) {
     I+= (eta * P - gamma*I)*dt;
     R+= (gamma*I + kappa * A)*dt;
     A+= ((1-omega) * sigma * E - kappa * A) * dt;
-    Z+= alpha*Z*dW;
+    Z+= (nu * (upsilon - Z)) * dt + sqrt(alpha)*Z*dW;
     C+= (eta*P)*dt;
-  ") -> SEI3R_GBM_binom_step2
+  ") -> SEI3R_CIR_step
   
-  par_names <- names(params)
+  par_names <- names(pars)
   
   obs_df %>% 
     pomp(
       times = "time", t0 = 0,
       rinit = rinit,
-      rprocess = pomp::euler(SEI3R_GBM_binom_step2, delta.t = dt),
+      rprocess = pomp::euler(SEI3R_CIR_step, delta.t = dt),
       statenames = c("S", "E", "P","I", "R", "A", "C", "Z"),
       paramnames = par_names,
       params = params,
       accumvars = "C",
       rmeasure = rmeas,
       dmeasure = dmeas,
-      partrans = parameter_trans(log   = c("zeta", "P_0", "tau", "alpha"),
-                                 logit = c("rho")),
+      partrans = parameter_trans(log   = c("zeta", "P_0", "tau", "nu", 
+                                           "upsilon", "alpha")),
       obsnames = c("y1", "y2"),
       cdir = ".", 
-      cfile= "SEI3R_GBM2_binom"
-    ) -> SEI3R_GBM2_binom
+      cfile= "SEI3R_CIR"
+    ) -> SEI3R_CIR
 }
-
-
-
