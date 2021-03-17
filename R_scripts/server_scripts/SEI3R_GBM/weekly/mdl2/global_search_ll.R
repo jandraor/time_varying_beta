c5f9bb5311be51e0949a4659006407fb97abbefa
Jair Andrade <jair.albert.andrade@gmail.com>
2021-03-17 15:08:18 +0000

    Update global_search_ll.R

--- R_scripts/server_scripts/SEI3R_GBM/weekly/mdl2/global_search_ll.R
+++ R_scripts/server_scripts/SEI3R_GBM/weekly/mdl2/global_search_ll.R
@@ -1,38 +1,11 @@
-library(deSolve)
-library(doParallel)
-library(doRNG)
-library(dplyr)
-library(imputeTS)
-library(lubridate)
-library(pomp)
-library(purrr)
-library(readr)
-library(readxl)
-library(stringr)
-library(tictoc)
-library(tidyr)
-
-folder <- "./Saved_objects/Irish_data/SEI3R_GBM/weekly/model_2"
-
-source("R_scripts/get_weekly_df.R")
-wkl_df <- get_weekly_df()
-
-#===============================================================================
-# Model setup
-#===============================================================================
-
-source("./R_scripts/POMP_models.R")
-
-par_obj  <- get_params("GBM_2")
-params   <- par_obj$all
-pomp_mdl <- pomp_SEI3R_GBM2(wkl_df, params, 1 / 128)
+source("./R_scripts/model_setup_GBM.R")
 
 #===============================================================================
 # Global search likelihood
 #===============================================================================
 source("./R_scripts/helpers.R")
-fn_gs     <- file.path(folder, "Global_search_mdl2.rds")
-gs_obj <- readRDS(fn_gs)
+fn_gs       <- file.path(folder, "Global_search_mdl2.rds")
+gs_obj      <- readRDS(fn_gs)
 mifs_global <- extract_mif_results(gs_obj)
 
 source("./R_scripts/likelihood_funs.R")
