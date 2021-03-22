e3d00fff5f440d000da93cd22e918269518235a2
Jair Andrade <jair.albert.andrade@gmail.com>
2021-03-22 23:12:20 +0000

    Update model_setup_GBM.R

--- R_scripts/model_setup_GBM.R
+++ R_scripts/model_setup_GBM.R
@@ -23,6 +23,7 @@ wkl_df <- get_weekly_df()
 
 source("./R_scripts/POMP_models.R")
 
-par_obj  <- get_params("GBM_2")
-params   <- par_obj$all
-pomp_mdl <- pomp_SEI3R_GBM2(wkl_df, params, 1 / 128)
+par_obj      <- get_params("GBM_2")
+params       <- par_obj$all
+fixed_params <- par_obj$fixed
+pomp_mdl     <- pomp_SEI3R_GBM2(wkl_df, params, 1 / 128)
