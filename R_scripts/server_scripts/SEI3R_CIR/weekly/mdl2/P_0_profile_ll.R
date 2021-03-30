a5e2bc6c7528e93b3211921af5920db1b70337be
Jair Andrade <jair.albert.andrade@gmail.com>
2021-03-30 22:23:20 +0100

    Add P_0 profile script CIR

---
+++ R_scripts/server_scripts/SEI3R_CIR/weekly/mdl2/P_0_profile_ll.R
@@ -0,0 +1,19 @@
+source("./R_scripts/model_setup_CIR.R")
+
+#===============================================================================
+# P(0) profile 
+#===============================================================================
+
+#===============================================================================
+var_name <- "P_0"
+#===============================================================================
+fn_ifp   <- file.path(folder, str_glue("ifp_{var_name}.rds"))
+ifp_obj  <- readRDS(fn_ifp)
+
+source("./R_scripts/helpers.R")
+profile_mif_results <- extract_mif_results(ifp_obj)
+
+source("./R_scripts/likelihood_funs.R")
+fn              <- file.path(folder, str_glue("ifp_{var_name}_ll_mdl2.rds"))
+profile_results <- mif_ll(profile_mif_results, seed = 879588259, 
+                          n_cores = 7, filename = fn)
