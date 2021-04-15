78a56b9960d8cfb1aabd27f6210eb427371689ec
Jair Andrade <jair.albert.andrade@gmail.com>
2021-04-15 17:55:46 +0100

    Add first iteration of SEI3R_4th

--- R_scripts/helpers.R
+++ R_scripts/helpers.R
@@ -68,3 +68,9 @@ extract_ll_df <- function(ll_output) {
   
   do.call("rbind", valid_results)
 }
+
+create_stan_file <- function(stan_text, filename) {
+  file_connection <- file(filename)
+  writeLines(stan_text, file_connection)
+  close(file_connection)
+}
