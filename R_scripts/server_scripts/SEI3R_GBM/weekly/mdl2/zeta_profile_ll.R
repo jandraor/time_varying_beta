5b72792d20d2949291cb46442c6c2126636aa2f7
Jair Andrade <jair.albert.andrade@gmail.com>
2021-04-22 14:40:30 +0100

    Update zeta_profile_ll.R

--- R_scripts/server_scripts/SEI3R_GBM/weekly/mdl2/zeta_profile_ll.R
+++ R_scripts/server_scripts/SEI3R_GBM/weekly/mdl2/zeta_profile_ll.R
@@ -1,32 +1,6 @@
-library(deSolve)
-library(doParallel)
-library(doRNG)
-library(dplyr)
-library(imputeTS)
-library(lubridate)
-library(pomp)
-library(purrr)
-library(readr)
-library(readxl)
-library(stringr)
-library(tictoc)
-library(tidyr)
+source("./R_scripts/model_setup_GBM.R")
 
-folder <- "./Saved_objects/Irish_data/SEI3R_GBM/weekly/model_2"
-
-source("R_scripts/get_weekly_df.R")
-wkl_df <- get_weekly_df()
-
-#===============================================================================
-# Model setup
-#===============================================================================
-
-source("./R_scripts/POMP_models.R")
-
-par_obj      <- get_params("GBM_2")
-params       <- par_obj$all
-fixed_params <- par_obj$fixed
-pomp_mdl     <- pomp_SEI3R_GBM2(wkl_df, params, 1 / 128)
+var_name <- "zeta"
 
 #===============================================================================
 # Zeta profile log lik
