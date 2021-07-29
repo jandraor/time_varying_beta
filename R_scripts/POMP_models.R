4bd37b486abe1cec6a13146bcd2c72c8be339b87
Jair Andrade <jair.albert.andrade@gmail.com>
2021-07-29 18:10:46 +0100

    Add pf sensitivity GBM1

--- R_scripts/POMP_models.R
+++ R_scripts/POMP_models.R
@@ -23,7 +23,8 @@ get_params <- function(mdl_id) {
   iota_guess    <- 1
   
   if(mdl_id == "GBM_1") {
-    unknown_pars <- c(B_0 = B_guess, P_0 = P_guess, alpha = alpha_guess)
+    unknown_pars <- c(zeta = zeta_guess, P_0 = P_guess, tau = tau_guess,
+                      alpha = alpha_guess)
   }
   
   if(mdl_id == "GBM_2") {
@@ -51,37 +52,38 @@ get_params <- function(mdl_id) {
        all = params)
 }
 
-get_POMP_model <- function(obs_df, params) {
+get_POMP_model <- function(obs_df, params, dt = 0.01) {
   
   Csnippet("
-    y = rpois(C);  
+    y1  = rpois(C);  
+    y2 = rnorm(Z, tau); 
   ") -> rmeas
   
   Csnippet("
-    lik = dpois(y,C,give_log) + 1e-5;
+    lik = dpois(y1,C,give_log) + dnorm(y2, Z, tau, give_log);
   ") -> dmeas
   
   Csnippet("
-    S = 4999970 - P_0;
+    S = N - P_0;
     E = 0;
     P = P_0;
     I = 0;
     A = 0;
     R = 0;
-    B = B_0;
+    Z = 1;
     C = 0;
   ") -> rinit
   
   Csnippet("
     double dW     = rnorm(0,sqrt(dt));
-    double lambda = B * (I + P + mu * A) / N;
+    double lambda = zeta * Z * (I + P + mu * A) / N;
     S-= (S * lambda)*dt;
     E+= (S * lambda - sigma*E)*dt;
     P+= (omega * sigma * E - eta * P) * dt;
     I+= (eta * P - gamma*I)*dt;
     R+= (gamma*I + kappa * A)*dt;
     A+= ((1-omega) * sigma * E - kappa * A) * dt;
-    B+= alpha*B*dW;
+    Z+= alpha*Z*dW;
     C+= (eta*P)*dt;
   ") -> SEI3R_GBM_step
   
@@ -91,14 +93,14 @@ get_POMP_model <- function(obs_df, params) {
     pomp(
       times = "time", t0 = 0,
       rinit = rinit,
-      rprocess = pomp::euler(SEI3R_GBM_step, delta.t = 0.01),
-      statenames = c("S", "E", "P","I", "R", "A", "C", "B"),
+      rprocess = pomp::euler(SEI3R_GBM_step, delta.t = dt),
+      statenames = c("S", "E", "P","I", "R", "A", "C", "Z"),
       paramnames = par_names,
       params = params,
       accumvars = "C",
       rmeasure = rmeas,
       dmeasure = dmeas,
-      partrans = parameter_trans(log   = c("B_0", "P_0", "alpha")),
+      partrans = parameter_trans(log   = c("zeta", "P_0", "tau", "alpha")),
       cdir = ".", 
       cfile= "SEI3R_GBM"
     ) -> SEI3R_GBM
